42ea852e5c4c28122b1b44a4f38e46c5647ec8bb
diegoc <diegoccy@users.noreply.github.com>
2021-09-03 03:35:24 -0400

    Update to chinese attack
    - added new transformation for chinese
    - added chinese word segmenter for data preprocessing
    - updated notebook tutorial for chinese attack

---
+++ textattack/segmenters/chinese_segmenter.py
@@ -0,0 +1,17 @@
+from pywordseg import *
+
+class ChineseSegmenter():
+
+    def segment_data(self, data):
+        seg = Wordseg(batch_size=64, device="cpu", embedding='elmo', elmo_use_cuda=False, mode="TW")
+
+        segmented = seg.cut([data["text"]])
+
+        processed = ""
+
+        for i in range(0, len(segmented)):
+            for j in range(0, len(segmented[i])):
+                processed = processed + segmented[i][j] + " "
+        data["text"] = processed
+
+        return data
